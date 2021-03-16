03323ce7ef9d2be37146468ab19bfb12cafc8f8f
siphera <salmansiphenkosi@gmail.com>
2021-03-16 10:16:01 +0200

    sr edit

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
-    origin: 'bottom',
+    origin: 'left',
     distance: '80px',
     duration: 2000,
     reset: true
